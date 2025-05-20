bef95566f92eb0859f09bd6b5be0b8bb247d1cc9
Nihal5014 <Nihal23.banda@gmail.com>
2025-05-20 19:14:39 +0100

    Add files via upload

--- space invader.py
+++ space invader.py
@@ -31,6 +31,13 @@ sprites = pygame.sprite.Group()
 sprites.add(spaceship_y)
 sprites.add(spaceship_r)
 
+red_bullets = []
+yellow_bullets = []
+def handle_bullets():
+    for bullet in yellow_bullets:
+        pygame.draw.rect(screen,"yellow",bullet)
+    for bullet in red_bullets:
+        pygame.draw.rect(screen,"red",bullet) 
 
 
 while run == True:
@@ -40,7 +47,16 @@ while run == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                bullet = pygame.Rect(spaceship_y.rect.x,spaceship_y.rect.y,15,5)
+                yellow_bullets.append(bullet)
+        if event.type == pygame.KEYDOWN:
+            if event.key ==pygame.K_m:
+                bullet2 =  pygame.Rect(spaceship_r.rect.x,spaceship_r.rect.y,15,5)
+                red_bullets.append(bullet2)
+    handle_bullets()
+
     key_press = pygame.key.get_pressed()
     if key_press[pygame.K_UP]:
         spaceship_r.rect.y = spaceship_r.rect.y-1
@@ -51,6 +67,37 @@ while run == True:
     if key_press[pygame.K_RIGHT]:
         spaceship_r.rect.x = spaceship_r.rect.x+1
     
+    key_press = pygame.key.get_pressed()
+    if key_press[pygame.K_w]:
+         spaceship_y.rect.y = spaceship_y.rect.y-1
+    if key_press[pygame.K_s]:
+        spaceship_y.rect.y = spaceship_y.rect.y+1
+    if key_press[pygame.K_a]:
+        spaceship_y.rect.x = spaceship_y.rect.x-1 
+    if key_press[pygame.K_d]:
+        spaceship_y.rect.x = spaceship_y.rect.x+1
+    
+    if spaceship_y.rect.top < 0:
+        spaceship_y.rect.top = 0
+    if spaceship_y.rect.left < 0:
+        spaceship_y.rect.left = 0
+    if spaceship_y.rect.bottom > 800:
+        spaceship_y.rect.bottom = 800
+    if spaceship_y.rect.right>500:
+        spaceship_y.rect.right = 500
+    
+    if spaceship_r.rect.top < 0:
+        spaceship_r.rect.top = 0
+    if spaceship_r.rect.left < 500:
+        spaceship_r.rect.left = 500
+    if spaceship_r.rect.bottom >800:
+        spaceship_r.rect.bottom = 800
+    if spaceship_r.rect.right > 1000:
+        spaceship_r.rect.right = 1000
 
 
+        
+    
+    
+    
     pygame.display.update()
